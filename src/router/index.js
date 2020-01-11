ab73818f1b1dc3f88e21f5c7fbaf7aad328dc89f
xxx <xxx>
2020-01-11 20:27:33 +0800

    使用vant

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import home from '@/views/home'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,6 +9,14 @@ const routes = [
     name: 'home',
     component: home
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
