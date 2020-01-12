58ecfb9a8fdae02e197b5dec8ca5e7d94649e570
xxx <xxx>
2020-01-12 20:49:55 +0800

    登录模块-进行登录

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './permission'
+import router from './permission' // 经过权限处理的router
 import store from './store'
 import Vant from 'vant' // 引入 vant组件库
-import 'vant/lib/index.less' // 样式文件
+import plugin from '@/utils/plugin'
+import 'vant/lib/index.less' // vant 样式文件 换成less
 import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
-import 'amfe-flexible'
+import 'amfe-flexible' // 引入自动适配插件
 Vue.use(Vant) // 一旦完成注册  在任意位置就可以使用 Vant组件库的组件
+Vue.use(plugin) // 注册小插件 会调用插件中的install方法
 Vue.config.productionTip = false
 
 new Vue({
