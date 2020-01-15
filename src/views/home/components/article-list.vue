8837e76944eda4135481b14db1f303264cc023ef
xxx <xxx>
2020-01-15 20:08:16 +0800

    article-list实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -2,8 +2,13 @@
   <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
   <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
   <div class="scroll-wrapper">
-    <van-list v-model="upLoading" :finished="finished" @load="onLoad"></van-list>
-    <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
+      <!-- 放置list组件list组件可以实现 上拉加载 -->
+      <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad">
+        <!-- v-for 渲染数据 -->
+        <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -12,28 +17,46 @@ export default {
   name: 'article-list',
   data () {
     return {
-      upLoading: false, // 是否加载数据
-      finished: false, // 加载是否完成
-      articles: [] // 定义一个数组
+      downLoading: false, // 是否开启下拉刷新状态
+      upLoading: false, // 是否开启上拉加载
+      finished: false, // 是否已经完成全部的数据加载
+      articles: [], // 定义一个数据来接收上拉加载的数据
+      refreshSuccessText: '' // 下拉成功显示的文本
     }
   },
   methods: {
+    //   上拉加载方法
     onLoad () {
-    // 加载方法
+      console.log('获取数据')
       setTimeout(() => {
-        if (this.articles.length === 50) {
-          // 停止追加
-          this.finished = true
+        //   给数据设置一个上限  不超过50条 如果50 条
+        if (this.articles.length < 50) {
+          let arr = Array.from(
+            Array(10),
+            (value, index) => this.articles.length + index + 1
+          )
+          this.articles.push(...arr)
+          //   关掉状态
+          this.upLoading = false
         } else {
-          let arr = Array.from(Array(10), (value, index) => index + this.articles.length + 1)
-          this.articles.push(...arr) // 把生成的数据追加到末尾
-          this.upLoading = false // 关闭状态
+          this.finished = true // 告诉list组件  我已经加载完了 不要再去触发onLoad事件了
         }
+      }, 1000)
+    },
+    // 下拉刷新方法
+    onRefresh () {
+      // 触发下拉刷新
+      console.log('下拉刷新')
+      setTimeout(() => {
+        let arr = Array.from(Array(10), (value, index) => ('追加' + (index + 1)))
+        this.articles.unshift(...arr) // 将数据添加到队首
+        this.downLoading = false // 关掉下拉状态
+        this.refreshSuccessText = `更新了${arr.length}条数据`
       }, 1000)
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 </style>
